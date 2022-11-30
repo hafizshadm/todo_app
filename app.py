bebc0771893e2d77e427e80568ca1167ccb770f7
Shad Muhammad <hafizshad.pk@gmail.com>
2022-11-30 13:02:53 +0500

    Rendering Data From .py In .html

--- app.py
+++ app.py
@@ -1,1 +1,25 @@
 # Application Is Powered by FLASK (Python)
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html', data=[
+        {
+            'description': 'Todo 1'
+        },
+        {
+            'description': 'Todo 2'
+        },
+        {
+            'description': 'Todo 3'
+        },
+        {
+            'description': 'Todo 4'
+        }
+    ])
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host="0.0.0.0", port=5000)
